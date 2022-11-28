cc34394b99d296fedb0f51761bff3834c2ffed74
Pavel Rei <mosbeytedd@mail.ru>
2022-11-29 00:48:49 +0300

    not sloved task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -3,5 +3,12 @@
 // 82 -> 10
 // 9012 -> 12
 Console.WriteLine("Введите число: ");
-int number = Convert,ToInt32(Console.ReadLine());
-string str = number.ToString();
+int number = Convert.ToInt32(Console.ReadLine());
+string A = number.ToString();
+int sum = 0;
+for(int i = 0; i < A.Length; i++)
+{
+   sum = sum + A[i];
+}
+
+Console.WriteLine("Сумма всех элементов числа равна: " + sum);
