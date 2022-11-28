beaee6595dc6f73a583e574f21bf4527081d765f
Pavel Rei <mosbeytedd@mail.ru>
2022-11-28 22:26:05 +0300

    Finally solved task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,3 +1,16 @@
 ﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int mult = numberA;
+
+for(int i =1; i < numberB; i++)
+{
+    mult = mult * numberA;
+}
+Console.WriteLine($"А в степени В равно {mult}");
--- Task27/Program.cs
+++ Task27/Program.cs
@@ -2,3 +2,6 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+Console.WriteLine("Введите число: ");
+int number = Convert,ToInt32(Console.ReadLine());
+string str = number.ToString();
