fe92c24b33bac91ebb901c573594740823f064bd
Pavel Rei <mosbeytedd@mail.ru>
2022-12-25 23:16:31 +0300

    Task55_Seminar

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 55: Задайте двумерный массив.
+// Напишите программу, которая заменяте тсроки на столбцы.
+
+int[,] FillMatrix(int rows, int columns)
+{
+    int[,] matrix = new int [rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = FillMatrix(m,n);
+PrintMatrix(matrix);
+Console.WriteLine();
+int [,] resultMatrix = new int[n,m];
+for(int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            resultMatrix[i,j] = matrix[j,i];
+        }
+        Console.WriteLine();
+    }
+PrintMatrix(resultMatrix);
